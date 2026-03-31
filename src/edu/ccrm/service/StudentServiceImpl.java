4e3b9cd0dda780365fcb45b0868268a9865a5664
AbhiralJain07 <jainabhiral7@gmail.com>
2026-03-31 19:09:06 +0530

    Initial project setup - Campus Course & Records Manager

---
+++ src/edu/ccrm/service/StudentServiceImpl.java
@@ -0,0 +1,17 @@
+package edu.ccrm.service;
+import edu.ccrm.domain.Student;
+import java.util.Collection;
+public class StudentServiceImpl implements StudentService {
+@Override
+public void addStudent(Student student) {
+DataStore.students.put(student.getRegNo(), student);
+}
+@Override
+public Student findStudentByRegNo(String regNo) {
+return DataStore.students.get(regNo);
+}
+@Override
+public Collection<Student> getAllStudents() {
+return DataStore.students.values();
+}
+}
